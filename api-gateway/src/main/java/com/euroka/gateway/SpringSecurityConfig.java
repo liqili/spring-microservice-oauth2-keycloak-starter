0224239b0912da1ef326991bb4827974be0aa7f8
Kunkka Li <liqili2005@gmail.com>
2020-10-14 21:34:15 +1300

    remove unused code

--- api-gateway/src/main/java/com/euroka/gateway/SpringSecurityConfig.java
+++ api-gateway/src/main/java/com/euroka/gateway/SpringSecurityConfig.java
@@ -1,23 +1,4 @@
 package com.euroka.gateway;
-//
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//
-//@Configuration
-//public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
-//
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http.authorizeRequests()
-//                .antMatchers("/login**","/callback/", "/webjars/**", "/error**", "/oauth2/authorization/**")
-//                .permitAll()
-//                .anyRequest()
-//                .authenticated()
-//                .and()
-//                .oauth2Login();
-//    }
-//}
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
